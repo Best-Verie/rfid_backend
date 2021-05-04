855bd3066a3fdea7e34fabb74d7c40974638032e
Best Verie Iradukunda <60257249+Best-Verie@users.noreply.github.com>
2021-05-04 15:10:58 +0200

    initial setup

---
+++ src/main/java/com/rfid/rfid_backend/RfidBackendApplication.java
@@ -0,0 +1,13 @@
+package com.rfid.rfid_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RfidBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RfidBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rfid/rfid_backend/RfidBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rfid.rfid_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RfidBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
